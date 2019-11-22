2c75724e978de5fe0a37b4b0e604b31cfa6e7113
Russ Taylor <russt@etsy.com>
2019-11-22 13:40:02 -0800

    Starting my very first attempt.

--- binary.go
+++ binary.go
@@ -1,1 +1,5 @@
 package binary_search
+
+func BinarySearch(target int, searchArray []int) int {
+	return -1
+}
--- binary_test.go
+++ binary_test.go
@@ -8,3 +8,16 @@ import (
 func TestAssert(t *testing.T) {
 	assert.Equal(t, 0, 0)
 }
+
+func TestBinarySearchReturnsNegativeOneIfTargetNotFound(t *testing.T) {
+	// Arrange
+	inputArray := []int{3, 4, 5}
+	searchTarget := 1
+	expected := -1
+
+	// Act
+	actual := BinarySearch(searchTarget, inputArray)
+
+	// Assert
+	assert.Equal(t, expected, actual)
+}
