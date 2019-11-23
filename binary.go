6badce9b593dce9a9dabeecb3a4d1ea5db6cfb54
Russ Taylor <russ@russt.me>
2019-11-22 20:33:22 -0800

    Complete the basic tests!

--- binary.go
+++ binary.go
@@ -8,6 +8,8 @@ import (
 func BinarySearch(target int, searchArray []int) int {
 	searchArrayLength := len(searchArray)
 
+	fmt.Printf("Searching %v for %d...\n", searchArray, target)
+
 	// Check if we even need to search
 	if searchArrayLength == 1 {
 		if searchArray[0] == target {
@@ -16,10 +18,16 @@ func BinarySearch(target int, searchArray []int) int {
 		return -1
 	}
 
-	searchTarget := int(math.Ceil(float64(searchArrayLength) / float64(2)) - 1)
-	fmt.Println(searchTarget)
-	if searchArray[searchTarget] == target {
-		return searchTarget
+	searchIndex := int(math.Ceil(float64(searchArrayLength) / float64(2)) - 1)
+	searchValue := searchArray[searchIndex]
+
+	if searchValue == target {
+		return searchIndex
+	} else if searchValue > target {
+		return BinarySearch(target, searchArray[:searchIndex])
+	} else if searchValue < target {
+		subSearchIndex := BinarySearch(target, searchArray[searchIndex+ 1:])
+		return subSearchIndex + searchIndex + 1
 	}
 	return -1
 }
--- binary_test.go
+++ binary_test.go
@@ -9,7 +9,7 @@ func TestAssert(t *testing.T) {
 	assert.Equal(t, 0, 0)
 }
 
-func TestBinarySearchReturnsNegativeOneIfTargetNotFound(t *testing.T) {
+func TestBinarySearchReturnsNegative1IfTargetNotFound(t *testing.T) {
 	// Arrange
 	inputArray := []int{3, 4, 5}
 	searchTarget := 1
@@ -22,7 +22,7 @@ func TestBinarySearchReturnsNegativeOneIfTargetNotFound(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestBinarySearchReturns0IfOnlyElementInArrayMatches(t *testing.T) {
+func TestBinarySearchReturns0In1ElementArrayWithTarget(t *testing.T) {
 	// Arrange
 	inputArray := []int{3}
 	searchTarget := 3
@@ -35,7 +35,7 @@ func TestBinarySearchReturns0IfOnlyElementInArrayMatches(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestBinarySearchReturnsMiddleIndexIfMiddleMatches(t *testing.T) {
+func TestBinarySearchReturns1In3ElementArrayWhereMiddleIsTarget(t *testing.T) {
 	// Arrange
 	inputArray := []int{1, 3, 5}
 	searchTarget := 3
@@ -47,3 +47,44 @@ func TestBinarySearchReturnsMiddleIndexIfMiddleMatches(t *testing.T) {
 	// Assert
 	assert.Equal(t, expected, actual)
 }
+
+func TestBinarySearchReturns0In3ElementArrayWhereFirstElementIsTarget(t *testing.T) {
+	// Arrange
+	inputArray := []int{1, 3, 5}
+	searchTarget := 1
+	expected := 0
+
+	// Act
+	actual := BinarySearch(searchTarget, inputArray)
+
+	// Assert
+	assert.Equal(t, expected, actual)
+}
+
+func TestBinarySearchReturns2In3ElementArrayWhereLastElementIsTarget(t *testing.T) {
+	// Arrange
+	inputArray := []int{1, 3, 5}
+	searchTarget := 5
+	expected := 2
+
+	// Act
+	actual := BinarySearch(searchTarget, inputArray)
+
+	// Assert
+	assert.Equal(t, expected, actual)
+}
+
+func TestBinarySearchReturns2In8ElementArrayWhereThirdElementIsTarget(t *testing.T) {
+	// Arrange
+	inputArray := []int{1, 3, 5, 11, 32, 42, 64, 210}
+	searchTarget := 5
+	expected := 2
+
+	// Act
+	actual := BinarySearch(searchTarget, inputArray)
+
+	// Assert
+	assert.Equal(t, expected, actual)
+}
+
+// TODO Check for unsorted array
